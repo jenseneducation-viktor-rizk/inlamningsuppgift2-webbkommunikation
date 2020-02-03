e85e3dee5d6c3d4ad0928689ee7d2ab49b0609a5
Viktor Rizk <vikriz930511@student.jenseneducation.se>
2020-02-03 15:26:44 +0100

    updates

--- public/js/index.js
+++ public/js/index.js
@@ -5,24 +5,53 @@ const fetchProducts = async () => {
     .then(response => {
       return response.json();
     })
-    .then(data => {
-      console.log(data);
-      appendProducts(data);
+    .then(products => {
+      appendProducts(products);
     })
     .catch(error => {
       console.error(error);
     });
 };
 
-const appendProducts = async products => {
-  const container = document.createElement("div");
-
-  let specProd = document.createTextNode(products[0].name);
-
-  container.appendChild(specProd);
-  document.querySelector(".product-container").appendChild(container);
+const addToCartButton = (i, productContainer) => {
+  let buttonName = document.createTextNode("Add To Cart");
+  const cartButton = document.createElement("button");
+  cartButton.className = "button-cart";
+  cartButton.id = 1 + i;
+  cartButton.appendChild(buttonName);
+  productContainer.appendChild(cartButton);
 };
 
-document
-  .querySelector(".button-products")
-  .addEventListener("click", fetchProducts);
+const productElement = async (products, i, productContainer) => {
+  const prodNameElem = document.createElement("h2");
+  let prodName = document.createTextNode(products[i].name);
+  prodNameElem.appendChild(prodName);
+  productContainer.appendChild(prodNameElem);
+};
+
+const productPrice = async (products, i, productContainer) => {
+  const prodPriceElem = document.createElement("h3");
+  let prodPrice = document.createTextNode(products[i].price + " kr");
+  prodPriceElem.appendChild(prodPrice);
+  productContainer.appendChild(prodPriceElem);
+};
+
+const productImg = async (products, i, productContainer) => {
+  const imgContainer = document.createElement("img");
+  imgContainer.src = products[i].image;
+  productContainer.appendChild(imgContainer);
+};
+
+const appendProducts = async products => {
+  for (let i = 0; i < products.length; i++) {
+    const productContainer = document.createElement("div");
+    productContainer.className = "product";
+    productElement(products, i, productContainer);
+    productImg(products, i, productContainer);
+    productPrice(products, i, productContainer);
+    addToCartButton(i, productContainer);
+    document.querySelector(".product-container").appendChild(productContainer);
+  }
+};
+
+fetchProducts();
